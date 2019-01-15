cfe313d147f14a916813838de1b7b05d596dbcd3
Ryan Hoegg <ryan.hoegg@gmail.com>
2019-01-15 16:05:23 -0500

    fix junit tests for HTTP listener port

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -1,7 +1,8 @@
 package com.mulesoft.training;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.mule.api.MuleEvent;
 import org.mule.tck.junit4.FunctionalTestCase;
@@ -16,6 +17,11 @@ public class HelloMavenTest extends FunctionalTestCase {
     @Override
     protected String getConfigFile() {
         return "maven-project.xml";
+    }
+    
+    @BeforeClass
+    public static void setupPort() {
+    		System.setProperty("http.port", "18082");
     }
     
     @Test
